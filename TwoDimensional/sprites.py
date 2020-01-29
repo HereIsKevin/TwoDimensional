77182167deec5674f8005f0dd38ee81170ea6ef4
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-01-29 02:24:10 +0000

    Begin on line sprite

--- TwoDimensional/sprites.py
+++ TwoDimensional/sprites.py
@@ -3,11 +3,50 @@ from PIL import ImageTk as PILImageTk
 
 
 class Sprite(object):
-    pass
+    def __init__(self, parent):
+        self._parent = parent
+        self._tag = None
+        self._position = [0, 0]
+    
+    @property
+    def parent(self):
+        return self._parent
+
+    @parent.setter
+    def parent(self, value):
+        self._parent = value
+
+    @property
+    def tag(self):
+        return self._tag
+
+    @tag.setter
+    def tag(self, value):
+        self._tag = value
+
+    def position(self):
+        return self._position
+
+    def destory(self):
+        self._parent._canvas.delete(self._tag)
+        self._parent.update()
+
+    def move_to(self, position):
+        self._parent._canvas.coords(self.tag, *position)
+        self._parent.update()
+        self._position = position
+
+    def move_by(self, position):
+        self._parent._canvas.move(self._tag, *position)
+        self._parent.update()
+        self._position[0] += position[0]
+        self._position[1] += position[1]
 
 
 class Image(Sprite):
     def __init__(self, parent, path, dimensions=[None, None], antialias=True):
+        super().__init__(parent)
+
         self._path = path
         self._image = PILImage.open(self._path)
         self._antialias = antialias
@@ -27,9 +66,6 @@ class Image(Sprite):
                 )
 
         self._tkimage = PILImageTk.PhotoImage(self._image)
-        self._parent = parent
-        self._tag = None
-        self._position = [0, 0]
 
     @property
     def image(self):
@@ -66,43 +102,21 @@ class Image(Sprite):
             self._image = self._image.resize(
                 self._dimensions, PILImage.ANTIALIAS
             )
-
-    @property
-    def parent(self):
-        return self._parent
-
-    @parent.setter
-    def parent(self, value):
-        self._parent = value
-
-    @property
-    def tag(self):
-        return self._tag
-
-    @tag.setter
-    def tag(self, value):
-        self._tag = value
-
-    def position(self):
-        return self._position
-
+    
     def draw(self):
         self._tag = self._parent._canvas.create_image(
             *self._position, image=self._tkimage
         )
         self._parent.update()
 
-    def destory(self):
-        self._parent._canvas.delete(self._tag)
-        self._parent.update()
 
-    def move_to(self, position):
-        self._parent._canvas.coords(self.tag, *position)
-        self._parent.update()
-        self._position = position
 
-    def move_by(self, position):
-        self._parent._canvas.move(self._tag, *position)
-        self._parent.update()
-        self._position[0] += position[0]
-        self._position[1] += position[1]
+class Line(Sprite):
+    def __init__(self, parent, coords, fill="black"):
+        super().__init__(parent)
+
+        self._coords = coords
+        self._fill = fill
+    
+    def draw(self):
+        self._tag = self._parent._canvas.create_line(*self._coords, fill=self._fill)
